11e1c0fd24e47e0c8dad37c839877559fe943d72
nAguayoValdebenito <145801865+nAguayoValdebenito@users.noreply.github.com>
2024-12-15 21:41:13 -0300

    Release1.2.0
    Release1.2.0

--- src/app/page/lista-alumnos/lista-alumnos.page.ts
+++ src/app/page/lista-alumnos/lista-alumnos.page.ts
@@ -3,6 +3,7 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 import { Asignaturas } from 'src/app/interfaces/asignaturas';
 import { Users } from 'src/app/interfaces/users';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-alumnos',
@@ -10,21 +11,40 @@ import { Router } from '@angular/router';
   styleUrls: ['./lista-alumnos.page.scss'],
 })
 export class ListaAlumnosPage implements OnInit {
-  asignaturasList: Asignaturas[] = []; // Lista de asignaturas
-  usuariosList: Users[] = []; // Lista de usuarios
-  asignaturasConUsuarios: { asignatura: Asignaturas; usuarios: Users[] }[] = []; // Datos combinados
+  asignaturasList: Asignaturas[] = [];
+  usuariosList: Users[] = [];
+  asignaturasConUsuarios: { asignatura: Asignaturas; usuarios: Users[] }[] = [];
+  isLoading = true;
 
-  constructor(private firebaseService: FirebaseService, private router: Router) {}
+  constructor(
+    private firebaseService: FirebaseService,
+    private router: Router,
+    private loadingController: LoadingController
+  ) {}
 
-  ngOnInit() {
-    this.loadAsignaturasYUsuarios();
+  async ngOnInit() {
+    // Mostrar pantalla de carga
+    const loading = await this.loadingController.create({
+      message: 'Cargando datos...',
+    });
+    await loading.present();
+
+    // Simulamos el tiempo fijo de carga (2 segundos)
+    setTimeout(async () => {
+      // Una vez que pase el tiempo, cargamos los datos
+      this.loadAsignaturasYUsuarios();
+
+      // Ocultamos el loader
+      await loading.dismiss();
+      this.isLoading = false; // Cambiamos el estado de carga
+    }, 2000); // Tiempo fijo de espera (2 segundos)
   }
 
   navigateToHome() {
     this.router.navigate(['/docente']);
   }
 
-  // Cargar asignaturas y usuarios
+  // Cargar asignaturas y usuarios desde Firebase
   loadAsignaturasYUsuarios() {
     this.firebaseService.getAsignaturas().subscribe(asignaturas => {
       this.asignaturasList = asignaturas;
